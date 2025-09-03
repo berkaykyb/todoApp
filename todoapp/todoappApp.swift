2a3541c1e06303b415be9d96e3a81b1baaab71b5
berkaykyb <berkayabasi2003@hotmail.com>
2025-09-03 14:08:40 +0300

    Initial Commit

---
+++ todoapp/todoappApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoappApp.swift
+//  todoapp
+//
+//  Created by Berkay KAYABAŞI on 3.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct todoappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ todoappTests/todoappTests.swift
@@ -0,0 +1,17 @@
+//
+//  todoappTests.swift
+//  todoappTests
+//
+//  Created by Berkay KAYABAŞI on 3.09.2025.
+//
+
+import Testing
+@testable import todoapp
+
+struct todoappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
